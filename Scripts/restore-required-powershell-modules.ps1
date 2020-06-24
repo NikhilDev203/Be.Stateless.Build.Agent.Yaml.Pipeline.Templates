8bc4a193fab59a5b5c9fd6465c3badb1a8dc260f
François Chabot <fchabot@skynet.be>
2020-06-24 17:46:30 +0200

    fix restore pwsh modules

--- Scripts/restore-required-powershell-modules.ps1
+++ Scripts/restore-required-powershell-modules.ps1
@@ -10,7 +10,8 @@ param(
 . $PSScriptRoot\powershell-module-functions.ps1
 
 $Path = Resolve-Path -Path $Path -ErrorAction Stop
-Get-ModuleManifest -Path $Path | Import-PowerShellDataFile | Select-Object -ExpandProperty RequiredModules | ForEach-Object -Process {
+$manifest = Get-ModuleManifest -Path $Path | Import-PowerShellDataFile
+$manifest.RequiredModules | ForEach-Object -Process {
     Write-Host "Importing module $_"
     Install-Module -Name $_ -Scope CurrentUser -Force
 }
